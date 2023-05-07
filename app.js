cdbf61c5772bb7a70026ad0cf92adaa04f0b7fb3
Axel Ponce <axelponce1997@gmail.com>
2023-05-06 22:04:40 -0300

    fix app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "/views/index.html"));
