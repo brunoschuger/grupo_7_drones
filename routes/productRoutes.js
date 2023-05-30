b30b354d3f207277489d177c5a521e1d34aac073
brunoschuger <schugerbruno@gmail.com>
2023-05-30 20:19:24 -0300

    creando json de productos y models. problema con filename de multer

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,18 @@ const express = require("express");
 const path = require("path");
 const router = express.Router(); 
 const productController = require('../controllers/productController')
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/images/uploads');
+    },
+    filename: (req, file, cb) => {
+        console.log(path.extname(file.originalname))
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
 
+const upload = multer({ storage });
 
 
 router.get("/:id/productDetail", productController.getProductDetail)
@@ -11,8 +22,7 @@ router.get("/productdetail-cursos", productController.geProductCursos);
 router.get("/productdetail-serviciotecnico", productController.getProductTecnico);
 router.get("/productdetail-accesorios", productController.getProductAccesorios);
 router.get("/productdetail-drones", productController.getProductDrones);
-
-router.post("/", productController.postProduct)
+router.post("/", upload.any('img'), productController.postProduct)
 router.get("/createProduct", productController.getCreate)
 
 
