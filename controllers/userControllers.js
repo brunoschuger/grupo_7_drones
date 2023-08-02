bc4532fdd4d0d454c33835a99234191ba064c9c4
brucer95 <scuteri26@gmail.com>
2023-08-02 03:49:01 -0300

    edicion de usuarios funcionando con base de datos

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,9 +2,9 @@ const path = require("path");
 const userModel = require("../models/user");
 const fs = require("fs");
 const expressValidator = require("express-validator");
-const bcrypt = require('bcrypt')
-const { User } = require('../database/models')
-const { v4: uuidv4 } = require('uuid')
+const bcrypt = require("bcrypt");
+const { User } = require("../database/models");
+const { v4: uuidv4 } = require("uuid");
 
 const controllers = {
 	getRegister: (req, res) => {
@@ -13,7 +13,7 @@ const controllers = {
 			logoRoute: "../images/logo-7drones.svg",
 			errors: [],
 			values: {},
-			user: req.session.user
+			user: req.session.user,
 		});
 	},
 	getCompleteUsers: (req, res) => {
@@ -22,61 +22,89 @@ const controllers = {
 			title: "Drones",
 			users,
 			logoRoute: "../images/logo-7drones.svg",
-			user: req.session.user
+			user: req.session.user,
 		});
 	},
 	registerUser: async (req, res) => {
-		const validation = expressValidator.validationResult(req)
-		console.log(validation); console.log(validation.errors)
+		const validation = expressValidator.validationResult(req);
+		console.log(validation);
+		console.log(validation.errors);
 		if (validation.errors.length > 0) {
-			return res.render('register', {
+			return res.render("register", {
 				title: "7 Drones - Registrate",
 				logoRoute: "../images/logo-7drones.svg",
 				errors: validation.errors,
 				values: req.body,
-				user: req.session.user
-			})
+				user: req.session.user,
+			});
 		}
 		const user = {
-			...req.body
+			...req.body,
 		};
-		
+
 		const newPassword = bcrypt.hashSync(user.password, 12);
 		/* user.confirmPassword = "" */
 		user.hashedpw = newPassword;
 		user.profileImg = "/images/uploads/profile-imgs/" + req.file.filename;
-		if (user.email.endsWith('@7drones.com.ar')) {
+		if (user.email.endsWith("@7drones.com.ar")) {
 			user.admin = true;
-		} else { user.admin = false }
+		} else {
+			user.admin = false;
+		}
 		user.uuid_id = uuidv4();
 		try {
 			User.create(user);
 
-			res.render('registerSuccess', {
+			res.render("registerSuccess", {
 				title: "Bienvenido",
 				logoRoute: "../images/logo-7drones.svg",
 				user: req.session.user,
-				nuevoUsuario: user.first_name
-			})
+				nuevoUsuario: user.first_name,
+			});
 		} catch (error) {
 			console.log(error);
 			res.send("Error en la creación del usuario.");
 		}
 	},
-	getUserProfile: (req, res) => {
-		const id = (req.params.id);
-		const usuarioAMostrar = req.session.user
-		console.log(usuarioAMostrar)
-		if (!usuarioAMostrar) {
-			return res.send("No se puede acceder al usuario");
+	getUserProfile: async (req, res) => {
+		try {
+			if (req.method === "GET") {
+				const userId = req.params.id;
+				const userFromDB = await User.findOne({
+					where: { id: userId },
+				});
+
+				if (!userFromDB) {
+					return res.send("No se puede acceder al usuario");
+				}
+
+				res.render("userProfile", {
+					title: "El perfil de " + userFromDB.first_name,
+					userFromDB,
+					logoRoute: "../../images/logo-7drones.svg",
+					user: req.session.user,
+				});
+			} else if (req.method === "POST") {
+				const userId = req.params.id;
+				const { username, email } = req.body;
+
+				const userFromDB = await User.findOne({
+					where: { id: userId },
+				});
+
+				if (!userFromDB) {
+					return res.send("No se puede acceder al usuario");
+				}
+
+				await userFromDB.update({ username, email });
+
+				res.redirect("/users/" + userId + "/userProfile");
+			}
+		} catch (error) {
+			console.log(error);
+			res.send("Error al obtener el perfil del usuario");
 		}
-		res.render("userProfile", {
-			title: "El perfil de" + req.session.user.first_name,
-			usuarioAMostrar,
-			logoRoute: "../../images/logo-7drones.svg",
-			user: req.session.user
-		});
-	}
-}
+	},
+};
 
 module.exports = controllers;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const path = require("path");
-const router = express.Router(); 
-const userControllers = require('../controllers/userControllers');
-const validar = require('../middleware/authregister');
-const upload = require ('../middleware/userImg'); 
-const { guestMiddleWare} = require("../middleware/acces")
+const router = express.Router();
+const userControllers = require("../controllers/userControllers");
+const validar = require("../middleware/authregister");
+const upload = require("../middleware/userImg");
+const { guestMiddleWare } = require("../middleware/acces");
 
-router.get("/:id/userProfile",  userControllers.getUserProfile)
+router.get("/:id/userProfile", userControllers.getUserProfile);
+router.post("/:id/userProfile", userControllers.getUserProfile);
 router.get("/register", guestMiddleWare, userControllers.getRegister);
 router.get("/completeUsers", userControllers.getCompleteUsers);
-router.post('/register', [ upload.single('profileImg'), validar.validacionesRegistro], userControllers.registerUser);
-
-
-
-
-
+router.post(
+	"/register",
+	[upload.single("profileImg"), validar.validacionesRegistro],
+	userControllers.registerUser
+);
 
 module.exports = router;
